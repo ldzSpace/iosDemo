3a368a288d1d9c7dcee838a7b764d9047c7171ce
刘大志 <842129948@qq.com>
2017-07-13 15:22:10 +0800

    Initial Commit

---
+++ iosdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosdemo
+//
+//  Created by 刘大志 on 2017/7/13.
+//  Copyright © 2017年 刘大志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosdemo
+//
+//  Created by 刘大志 on 2017/7/13.
+//  Copyright © 2017年 刘大志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
