d22381331dae6bb64d957a5305cc2bdff2e7551e
刘大志 <842129948@qq.com>
2017-07-13 17:39:15 +0800

    首次提交ios demo 1 添加了UILabel控件,学习其使用

--- iosdemo/ViewController.m
+++ iosdemo/ViewController.m
@@ -14,15 +14,41 @@
 
 @implementation ViewController
 
+-(void) create {
+	UILabel* label = [UILabel new];
+	// UILable的内容
+	label.text = @"hollo World!";
+	// UILabel文字的颜色
+	label.textColor = [UIColor blueColor];
+	// CGRect的参数分别为左上坐标,以及长和宽
+	label.frame = CGRectMake(100, 100, 160, 40);
+	// UILabel的背景色
+	//label.backgroundColor = [UIColor redColor];
+	// 设置字体的大小
+	label.font = [UIFont systemFontOfSize:12];
+	// 设置整个视图的背景色
+	//self.view.backgroundColor = [UIColor yellowColor];
+	// 添加控件到view,这view就是整个当前的视图,相当于android的activity
+	[self.view addSubview:label];
+	
+	// lable的高级属性
+	// 设置阴影的颜色
+	label.shadowColor = [UIColor grayColor];
+	// 设置阴影的偏移位置
+	label.shadowOffset = CGSizeMake(5, 5);
+}
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	// 调用创键UI函数
+	[self create];
 }
 
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+	
 }
 
 
