3dd0f3878fc83549ab67b5a9bf0c6e241af8bf85
刘大志 <842129948@qq.com>
2017-07-14 14:34:49 +0800

    进度条和滑动条的使用

--- iosdemo/ViewController02.h
+++ iosdemo/ViewController02.h
@@ -15,13 +15,24 @@
 	// 并通过此消息调用相应的事件函数
 	// 通过此函数可以在固定的时间段完成一个根据事件间隔的任务
 	NSTimer* _timeView;
+	UIButton* _myButton01;
+	UIButton* _myButton02;
 	UISwitch* _mySwitch;
+	UISlider* _mySlider;
+	UIProgressView* _myProgress;
 }
 
+// 按钮01属性对象
+@property (retain, nonatomic) UIButton* myButton01;
+// 按钮02属性对象
+@property (retain, nonatomic) UIButton* myButton02;
+// 滑动条的属性对象
+@property (retain, nonatomic) UISlider* mySlider;
 // shitch属性对象
 @property (retain, nonatomic) UISwitch* mySwitch;
-
 // 定时器的属性对象
 @property (retain, nonatomic) NSTimer* timeView;
+// 进度条的属性对象
+@property (retain, nonatomic) UIProgressView* myProgress;
 
 @end
--- iosdemo/ViewController02.m
+++ iosdemo/ViewController02.m
@@ -24,21 +24,31 @@
 	// 视图控制器2的背景
 	self.view.backgroundColor = [UIColor orangeColor];
 	
-	UIButton* btn = [UIButton buttonWithType: UIButtonTypeRoundedRect];
-	btn.frame = CGRectMake(150, 200, 80, 40);
-	[btn setTitle:@"启动定时器" forState:UIControlStateNormal];
-	[btn addTarget:self action:@selector(pressStart) forControlEvents:UIControlEventTouchUpInside];
+	[self createButton];
+	[self createSwitch];
+	[self createProgress];
+	[self createSilder];
+	[self.view addSubview:_myButton01];
+	[self.view addSubview:_myButton02];
+    [self.view addSubview: _mySwitch];
+	[self.view addSubview:_myProgress];
+	[self.view addSubview:_mySlider];
+}
+
+- (void) createButton {
+	_myButton01 = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+	_myButton01.frame = CGRectMake(150, 200, 80, 40);
+	[_myButton01 setTitle:@"启动定时器" forState:UIControlStateNormal];
+	[_myButton01 addTarget:self action:@selector(pressStart) forControlEvents:UIControlEventTouchUpInside];
 	
-	UIButton* btn01 = [UIButton buttonWithType: UIButtonTypeRoundedRect];
-	btn01.frame = CGRectMake(200, 100, 80, 40);
-	btn01.tag = 101;
-	[btn01 setTitle:@"停止定时器" forState:UIControlStateNormal];
-	[btn01 addTarget:self action:@selector(pressEnd) forControlEvents:UIControlEventTouchUpInside];
-	
-	[self.view addSubview:btn];
-	[self.view addSubview:btn01];
-	
-	
+	_myButton02 = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+	_myButton02.frame = CGRectMake(200, 100, 80, 40);
+	_myButton02.tag = 101;
+	[_myButton02 setTitle:@"停止定时器" forState:UIControlStateNormal];
+	[_myButton02 addTarget:self action:@selector(pressEnd) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) createSwitch {
 	// 创建一个开关控件
 	// 继承与UIView
 	_mySwitch = [[UISwitch alloc] init];
@@ -47,7 +57,6 @@
 	_mySwitch.on = YES;
 	// 也可以采用set方法
 	[_mySwitch setOn: YES];
-	[self.view addSubview: _mySwitch];
 	// 设置开启状态的风格颜色
 	[_mySwitch setOnTintColor:[UIColor redColor]];
 	// 设置开关圆按钮的颜色
@@ -58,7 +67,50 @@
 	[_mySwitch addTarget:self action:@selector(swChange:) forControlEvents:UIControlEventValueChanged];
 }
 
-- (void)swChange : (UISwitch*) sw {
+- (void) createProgress {
+	// 进度条的创建
+	_myProgress = [[UIProgressView alloc] init];
+	// 设置进度条的位置大小,同时进度条的高度是不可变化
+	_myProgress.frame = CGRectMake(10, 80, 200, 30);
+	// 设置进度 范围 0~1
+	_myProgress.progress = 0;
+	// 设置进度条的风格特征
+	_myProgress.progressViewStyle = UIProgressViewStyleDefault;
+	// 设置进度条的颜色
+	_myProgress.progressTintColor = [UIColor magentaColor];
+	// 设置进度条未达到的部分的颜色
+	_myProgress.trackTintColor = [UIColor blackColor];
+}
+
+// 设置滑动条
+- (void) createSilder {
+	// 创建滑动条
+	_mySlider = [[UISlider alloc] init];
+	// 滑动条位置的设置 ,高度是不可以改变的
+	_mySlider.frame = CGRectMake(10, 200, 200, 40);
+	// 设置滑动条的最大值
+	_mySlider.maximumValue = 100;
+	// 设置滑动条的最小值
+	_mySlider.minimumValue = 0;
+	// 设置滑动条的滑块位置float值
+	_mySlider.value = 0;
+	// 左侧滑动条的背景颜色
+	_mySlider.minimumTrackTintColor = [UIColor blackColor];
+	// 右侧滑动条的背景颜色
+	_mySlider.maximumTrackTintColor = [UIColor greenColor];
+	// 设置滑块的颜色
+	_mySlider.thumbTintColor = [UIColor purpleColor];
+	// 添加对滑动条的时间函数
+	[_mySlider addTarget:self action:@selector(slideProgress) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void) slideProgress {
+	_myProgress.progress = (_mySlider.value - _mySlider.minimumValue ) / (_mySlider.maximumValue - _mySlider.minimumValue);
+	NSLog(@"value = %f", _mySlider.value);
+	NSLog(@"progress = %f", _myProgress.progress);
+}
+
+- (void) swChange : (UISwitch*) sw {
 	NSLog(@"当前开关的状态 : %@",sw.isOn == YES ? @"开" : @"关");
 }
 
@@ -83,6 +135,7 @@
 	[_timeView invalidate];
 	NSLog(@"关闭计时器");
 }
+
 // 触摸时候触发的方法
 - (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 	// 先使当前的控制器消失掉
@@ -96,6 +149,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
